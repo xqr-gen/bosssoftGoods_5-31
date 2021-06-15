6e7781de13149178b07c3d7dd29b9094889bd501
xqr-gen <56260516+xqr-gen@users.noreply.github.com>
2021-06-15 11:44:55 +0800

    Update CartVO.java

--- src/main/java/com/bosssoft/springboot5_31/pojo/vo/CartVO.java
+++ src/main/java/com/bosssoft/springboot5_31/pojo/vo/CartVO.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CartVO {
+    @NotNull(message = "空的")
     private Integer cid;
+    @NotNull(message = "空的")
     private Integer uid;
 }
