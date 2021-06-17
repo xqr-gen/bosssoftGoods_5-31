acecaacc507834c9f46998a9cac75a6c3fac2828
xqr-gen <56260516+xqr-gen@users.noreply.github.com>
2021-06-17 20:28:38 +0800

    Update cart.sql

--- sql/cart.sql
+++ sql/cart.sql
@@ -1,1 +1,42 @@
-INSERT INTO `cart` (`cid`, `uid`, `gid`, `quantity`) VALUES (1, 1, 1, 1);
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80025
+ Source Host           : localhost:3306
+ Source Schema         : goods_copy
+
+ Target Server Type    : MySQL
+ Target Server Version : 80025
+ File Encoding         : 65001
+
+ Date: 17/06/2021 20:17:02
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cart
+-- ----------------------------
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE `cart`  (
+  `cid` int NOT NULL AUTO_INCREMENT,
+  `uid` int NOT NULL,
+  `gid` int NOT NULL,
+  `quantity` int NOT NULL,
+  `version` int NULL DEFAULT '1',
+  `status` int NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`cid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of cart
+-- ----------------------------
+INSERT INTO `cart` VALUES (1, 1, 1, 1, 1, 1, '2021-06-18 20:15:50', '2021-06-18 20:15:56');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
