b1c73a5afe830952f4156bc863f7a1874623daa5
xqr-gen <56260516+xqr-gen@users.noreply.github.com>
2021-06-17 20:30:32 +0800

    Update user.sql

--- sql/user.sql
+++ sql/user.sql
@@ -1,1 +1,37 @@
-INSERT INTO `user` (`uid`, `uname`, `pwd`) VALUES (1, 'xqr', '123456');
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80025
+ Source Host           : localhost:3306
+ Source Schema         : goods_copy
+
+ Target Server Type    : MySQL
+ Target Server Version : 80025
+ File Encoding         : 65001
+
+ Date: 17/06/2021 20:17:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `uname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `pwd` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'xqr', '123456');
+
+SET FOREIGN_KEY_CHECKS = 1;
+
