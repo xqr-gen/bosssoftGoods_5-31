3203e1dcbb27a64d154647587d928e67c9684790
xqr-gen <56260516+xqr-gen@users.noreply.github.com>
2021-06-17 20:29:33 +0800

    Update goods.sql

--- sql/goods.sql
+++ sql/goods.sql
@@ -1,5 +1,41 @@
-INSERT INTO `goods` (`gid`, `goodsName`, `price`, `quantity`) VALUES (1, '小酥肉', 7.00, 10);
-INSERT INTO `goods` (`gid`, `goodsName`, `price`, `quantity`) VALUES (2, '鸡排', 10.00, 10);
-INSERT INTO `goods` (`gid`, `goodsName`, `price`, `quantity`) VALUES (3, '牛肉汉堡', 12.00, 10);
-INSERT INTO `goods` (`gid`, `goodsName`, `price`, `quantity`) VALUES (4, '可乐', 3.50, 10);
-INSERT INTO `goods` (`gid`, `goodsName`, `price`, `quantity`) VALUES (5, '圣代', 7.00, 10);
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80025
+ Source Host           : localhost:3306
+ Source Schema         : goods_copy
+
+ Target Server Type    : MySQL
+ Target Server Version : 80025
+ File Encoding         : 65001
+
+ Date: 17/06/2021 20:17:12
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `gid` int NOT NULL AUTO_INCREMENT,
+  `goodsName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `price` decimal(15, 2) NOT NULL,
+  `quantity` int NULL DEFAULT NULL,
+  PRIMARY KEY (`gid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+INSERT INTO `goods` VALUES (1, '小酥肉', 7.00, 10);
+INSERT INTO `goods` VALUES (2, '鸡排', 10.00, 10);
+INSERT INTO `goods` VALUES (3, '牛肉汉堡', 12.00, 10);
+INSERT INTO `goods` VALUES (4, '可乐', 3.50, 10);
+INSERT INTO `goods` VALUES (5, '圣代', 7.00, 10);
+
+SET FOREIGN_KEY_CHECKS = 1;
