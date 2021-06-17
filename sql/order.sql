c73d38fea74ecca1c3d5f3a05e32b8be707c3b77
xqr-gen <56260516+xqr-gen@users.noreply.github.com>
2021-06-17 20:33:35 +0800

    Update order.sql

--- sql/order.sql
+++ sql/order.sql
@@ -1,1 +1,38 @@
-INSERT INTO `order` (`cid`, `uid`, `tPrice`, `payStatus`) VALUES (1, 1, 7.00, 0);
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : mysql8
+ Source Server Type    : MySQL
+ Source Server Version : 80025
+ Source Host           : localhost:3306
+ Source Schema         : goods_copy
+
+ Target Server Type    : MySQL
+ Target Server Version : 80025
+ File Encoding         : 65001
+
+ Date: 17/06/2021 20:17:22
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for order
+-- ----------------------------
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`  (
+  `cid` int NOT NULL AUTO_INCREMENT,
+  `uid` int NULL DEFAULT NULL,
+  `tPrice` decimal(15, 2) NULL DEFAULT NULL,
+  `payStatus` int NULL DEFAULT NULL,
+  PRIMARY KEY (`cid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of order
+-- ----------------------------
+INSERT INTO `order` VALUES (1, 1, 7.00, 0);
+
+SET FOREIGN_KEY_CHECKS = 1;
+
